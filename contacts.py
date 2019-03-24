44f436f26b30eadd5c37584a5fb006a22f4a9dc7
Alexander Rodionov <alx1dr@gmail.com>
2019-03-24 21:19:44 +0300

    Create contacts.py

---
+++ contacts.py
@@ -0,0 +1,7 @@
+contacts = dict(
+    telegram = 'tandav',
+    twitter  = 'tandavaya',
+    email    = 'alx1dr@gmail.com',
+    skype    = 'alexander21928',
+    youtube  = 'https://www.youtube.com/channel/UCbgKryv4gdpJJ7_Rg5i2xyg',
+)
