84d7bd8242e9823716e6fce8864a1b7b90a5467f
Alexander Rodionov <alx1dr@gmail.com>
2019-03-31 18:38:52 +0300

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,7 +1,11 @@
 contacts = dict(
-    telegram = 'tandav',
-    twitter  = 'tandavaya',
-    email    = 'alx1dr@gmail.com',
-    skype    = 'alexander21928',
-    youtube  = 'https://www.youtube.com/channel/UCbgKryv4gdpJJ7_Rg5i2xyg',
+    telegram   = 'tandav',
+    twitter    = 'tandavaya',
+    email      = 'alx1dr@gmail.com',
+    skype      = 'alexander21928',
+    youtube    = 'https://www.youtube.com/channel/UCbgKryv4gdpJJ7_Rg5i2xyg',
+    lastfm     = 'alx1dr',
+    github     = 'tandav',
+    soundcloud = 'thetandav',
+    spotify    = 'sfaklyytf1x59ig1h4q31vcd5', 
 )
