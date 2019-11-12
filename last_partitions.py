e01d5244607f7472ad6ce1b9be19c04362d36112
Alexander Rodionov <alx1dr@gmail.com>
2019-11-12 19:17:53 +0300

    Update last_partitions.py

--- last_partitions.py
+++ last_partitions.py
@@ -1,8 +1,10 @@
+import subprocess
+import pyspark.sql.functions as f
+
 def last_partitions(table, n=3):
     _ = ['hive', '-e', 'show partitions {}'.format(table)]
     _ = subprocess.check_output(_)
     _ = _.decode()
     _ = _.splitlines()
-    _ = _[1 : n + 1] # skip header
-    _ = [x.split('=')[1] for x in _]
-    return _
+    part_column, part_value = _[-n].split('=')
+    return f.col(part_column) >= part_value
