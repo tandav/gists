e7fddadba9f9856520e6f73f7c9096e31cd397c9
Alexander Rodionov <alx1dr@gmail.com>
2019-11-19 12:59:04 +0300

    Update sets_info.py

--- sets_info.py
+++ sets_info.py
@@ -3,15 +3,15 @@ def sets_info(table_a, table_b, col_a, col_b):
     print set informations about values in 2 columns
     usefull for various ids
     '''
-
     a = frozenset(spark.table(table_a).rdd.map(lambda x: getattr(x, col_a)).distinct().collect())
     b = frozenset(spark.table(table_b).rdd.map(lambda x: getattr(x, col_b)).distinct().collect())
     
     print('a = {}.{}'.format(table_a, col_a))
     print('b = {}.{}'.format(table_b, col_b))
+    print('len a:', len(a    ))
+    print('len b:', len(b    ))
     print('a | b:', len(a | b))
     print('a & b:', len(a & b))
     print('a ^ b:', len(a ^ b))
     print('a - b:', len(a - b))
     print('b - a:', len(b - a))
-    
