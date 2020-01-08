b686acbe6ea4ea5e5fe31671be3d0325f09743dd
Alexander Rodionov <alx1dr@gmail.com>
2020-01-08 18:20:22 +0300

    upd

--- random_music_track.py
+++ random_music_track.py
@@ -2,8 +2,15 @@ from pathlib import Path
 import random
 import plistlib
 import webbrowser
+import sys
 
-music = Path('/Users/tandav/Documents/spaces/music/music')
+if len(sys.argv) == 2:
+    music = Path(sys.argv[1])
+else:
+    music = Path('/Users/tandav/Documents/spaces/music/music')
+
+print(music.absolute())
+
 tracks = list(music.rglob('*.webloc'))
 
 track = random.choice(tracks)
