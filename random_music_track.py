7fea4f3b25456eefa513438867fce8271f2c7ff8
Alexander Rodionov <alx1dr@gmail.com>
2020-01-08 14:25:09 +0300

    better relative print

--- random_music_track.py
+++ random_music_track.py
@@ -7,7 +7,7 @@ music = Path('/Users/tandav/Documents/spaces/music/music')
 tracks = list(music.rglob('*.webloc'))
 
 track = random.choice(tracks)
-print(track.stem)
+print(track.relative_to(music))
 with open(track, 'rb') as fd:
     url = plistlib.load(fd)['URL']
 webbrowser.open(url)
