30bf6ff77733e4cc0cd08223588f4e2d29c584dd
Alexander Rodionov <alx1dr@gmail.com>
2020-01-08 14:10:33 +0300

    upd

--- random_blurred_background.py
+++ random_blurred_background.py
@@ -1,5 +1,5 @@
 import io
-import pathlib
+from pathlib import Path
 import subprocess
 import requests
 import PIL.Image
@@ -12,26 +12,14 @@ width, height = 1440, 900
 width  *= 2
 height *= 2
 
-img_file = pathlib.Path('blur.jpg').absolute()
+img_file = Path('blur.jpg').absolute()
 
-(
-    PIL
-    .Image
-    .open(
-        io
-        .BytesIO(
-            requests
-            .get(f'https://source.unsplash.com/random/{width}x{height}')
-            .content
-        )
-    )
-    .filter(
-        PIL
-        .ImageFilter
-        .GaussianBlur(radius=60)
-    )
-    .save(img_file)
-)
+_ = f'https://source.unsplash.com/random/{width}x{height}'
+_ = requests.get(_).content
+_ = io.BytesIO(_)
+_ = PIL.Image.open(_)
+_ = _.filter(PIL.ImageFilter.GaussianBlur(radius=60))
+_.save(img_file)
 
 set_wallpaper = f'''
 tell application "System Events"
@@ -42,3 +30,4 @@ end tell
 '''
 
 subprocess.run(['osascript', '-e', set_wallpaper])
+subprocess.run(['killall', 'Dock']) # sometimes you need this
