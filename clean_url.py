4aeb3eb42f30e0d7be7064c2fb5231605bb3f7ca
Alexander Rodionov <alx1dr@gmail.com>
2020-03-07 13:04:49 +0300

    add clean_url.py

---
+++ clean_url.py
@@ -0,0 +1,11 @@
+import sys
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
+
+def clean(url, allowed_keys=frozenset({'q', 'tbm'})):
+    o = urlparse(url)
+    params = parse_qs(o.query)
+    clean_params = {k: params[k] for k in allowed_keys}
+    clean_url = urlunparse(o._replace(query=urlencode(clean_params, doseq=True)))
+    return clean_url
+
+print(clean(sys.argv[1]))
