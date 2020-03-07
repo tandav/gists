bd8b6fb6a427c63a20e34d2d55ddba3df0a2850d
Alexander Rodionov <alx1dr@gmail.com>
2020-03-07 13:54:25 +0300

    k -> p

--- clean_url.py
+++ clean_url.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 def clean(url, allowed_params=('q', 'tbm')):
     o = urlparse(url)
     params = parse_qs(o.query)
-    clean_params = {k: params[k] for k in allowed_params}
+    clean_params = {p: params[p] for p in allowed_params}
     clean_url = urlunparse(o._replace(query=urlencode(clean_params, doseq=True)))
     return clean_url
 
