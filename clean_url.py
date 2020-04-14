e2fc487b112882ad471577b2fc184521e5eefe58
Alexander Rodionov <alx1dr@gmail.com>
2020-04-14 17:52:29 +0300

    drop useless https:// schema

--- clean_url.py
+++ clean_url.py
@@ -6,6 +6,11 @@ def clean(url, allowed_params=('q', 'tbm')):
     params = parse_qs(o.query)
     clean_params = {p: params[p] for p in params.keys() & allowed_params}
     clean_url = urlunparse(o._replace(query=urlencode(clean_params, doseq=True)))
+    
+    # drop https:// scheme part
+    clean_url = clean_url.partition('google.com')
+    clean_url = ''.join(clean_url[1:])
+    
     return clean_url
 
 print(clean(sys.argv[1]))
