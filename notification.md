0398485046837666361d61429d44200f5a45290f
Alexander Rodionov <alx1dr@gmail.com>
2020-04-19 01:26:53 +0300

    _

--- notification.md
+++
@@ -1,9 +0,0 @@
-```applescript
-display notification "Text" with title "Title" subtitle "Subtitle" sound name "Glass"
-```
-
-or 
-
-```sh
-osascript -e ' display notification "Text" with title "Title" subtitle "Subtitle" sound name "Glass"'
-```
--- notification.py
+++ notification.py
@@ -1,10 +1,12 @@
 import subprocess
 
-def notification(text=None, title=None, subtitle=None):
+def notification(text=None, title=None, subtitle=None, sound=False):
     code = f'display notification "{text}"' 
     if title:
         code += f'with title "{title}"'
     if subtitle:
         code += f'subtitle "{subtitle}"'
+    if sound:
+        code += 'sound name "Glass"' # more sounds: ls /System/Library/Sounds
     cmd  = 'osascript', '-e', code
     subprocess.run(cmd)
