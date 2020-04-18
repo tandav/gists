709a9b270d49ac755a16772d968bd15bba807dee
Alexander Rodionov <alx1dr@gmail.com>
2020-04-19 01:21:26 +0300

    _

--- notification.py
+++ notification.py
@@ -1,6 +1,10 @@
 import subprocess
 
-def notification(title, subtitle, text):
-    code = f'display notification "{text}" with title "{title}" subtitle "{subtitle}"'
+def notification(text=None, title=None, subtitle=None):
+    code = f'display notification "{text}"' 
+    if title:
+        code += f'with title "{title}"'
+    if subtitle:
+        code += f'subtitle "{subtitle}"'
     cmd  = 'osascript', '-e', code
     subprocess.run(cmd)
