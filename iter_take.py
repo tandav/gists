b471c56624261045dabf72c69135157d396d815f
Alexander Rodionov <alx1dr@gmail.com>
2020-07-15 22:13:18 +0300

    _

--- iter_take.py
+++ iter_take.py
@@ -1,4 +1,5 @@
 import itertools
 
+first      = lambda obj: next(iter(obj))
 take       = lambda obj, n: list(itertools.islice(obj        , n))
 take_items = lambda obj, n: list(itertools.islice(obj.items(), n))
