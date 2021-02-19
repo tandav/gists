3729b05daf5496c9240ba8fca85103e7683339b9
Alexander Rodionov <alx1dr@gmail.com>
2021-02-19 17:03:13 +0300

    _

--- ping.py
+++ ping.py
@@ -1,24 +1,24 @@
 import subprocess
-import time
 import shlex
 import re
 import macos
 
 
 router = subprocess.check_output('route get default | grep gateway', shell=True, text=True).strip().split()[1]
-cmd = f'sudo ping -f {router} -c 999'
+cmd = f'sudo ping -f {router} -c 1000'
 cmd = shlex.split(cmd)
 
 pattern = r'[\d|\.]+(?=%)'
 
-NOTIFICATION_TRESHOLD = 50 # %
+NOTIFICATION_TRESHOLD = 50
 
 while True:
     out = subprocess.check_output(cmd, text=True)
     a, b = out.splitlines()[-2:]
     print(b, a, sep=' | ')
+    a = a.rsplit(',', maxsplit=1)[1].strip()
+    b = b.split(maxsplit=1)[1]
     loss = float(re.search(pattern, a).group())
 
     if loss > NOTIFICATION_TRESHOLD:
-        macos.notification(text=f'{loss}%', title='poor connection', subtitle='packet loss')
-
+        macos.notification(text=b, title='poor connection', subtitle=a)
